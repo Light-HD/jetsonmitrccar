21d5f95f7a91f687e174968e6b7883b98ead8641
kaval <goeksu.kaval@gt-arc.com>
2019-08-08 01:09:08 +0200

    Clean and Share Newes

---
+++ SixWheelCar_Firmware/Custom/WTR_MOTOR_MCU/WTR_MOTOR_3/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+WildThumperLib_Motor.c
+
+Wild_Thumper_Motor.c
+
+WT_I2Cslave.c
+
--- morse/sixwd/src/sixwd/builder/actuators/__init__.py
+++
@@ -1,1 +0,0 @@
-from .basicspeed import Basicspeed
