78b9c7454def2b5fb86d6cd7aa5234f39023ba95
Jiyo Palatti <j.palatti@campus.tu-berlin.de>
2019-11-08 15:02:40 +0100

    Removed all unwanted packages

--- Six_Wheeled_Motor_Controller/Software/RobotLoader_20130410/robotloader_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-
-java -Xmx256m -Djava.library.path="./lib" -jar RobotLoader_lib.jar
--- Six_Wheeled_Motor_Controller/Software/RobotLoader_20130410/robotloader_linux_x64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-
-java -Xmx256m -Djava.library.path="./lib/linux_x64" -jar RobotLoader_lib.jar
