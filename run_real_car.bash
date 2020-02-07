b6b45f0044c02fc666a670f2c5d5b701a9910031
egeKeyvan <ege.keyvan@gmail.com>
2020-02-07 14:49:34 +0100

    final changes

--- run_real_car.bash
+++ run_real_car.bash
@@ -1,2 +1,2 @@
 source devel/setup.bash
-bash parallel "roslaunch bear_car_launch autonomous_navigation_fourwd_sim.launch is_simulation:=false use_ros_control:false" 
+bash parallel "roslaunch bear_car_launch autonomous_navigation_fourwd.launch is_simulation:=false use_ros_control:=true" 
